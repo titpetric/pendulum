76bc260bcc21e837195e381d0f1cb26a78fb2dc1
Tit Petric <black@scene-si.org>
2017-08-21 10:36:43 +0200

    update root fs

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f pendulum
-docker run --name pendulum --restart=always -p 8080:8080 -d -v $(pwd)/contents:/app/public_html/contents titpetric/pendulum -port 8080
+docker run --name pendulum --restart=always -p 8080:8080 -d -v $(pwd)/contents:/app/contents titpetric/pendulum -port 8080
