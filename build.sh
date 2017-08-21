aa09d1a91b86732f35107de5a25fa3340dda534a
Tit Petric <black@scene-si.org>
2017-08-21 10:35:56 +0200

    add assetfs dep

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if [ ! -d "vendor/github.com/elazarl/go-bindata-assetfs" ]; then
+	gvt fetch github.com/elazarl/go-bindata-assetfs
+fi
+go generate
 docker run --rm -it -e CGO_ENABLED=0 -v `pwd`:/go/src/app -w /go/src/app golang:1.8-alpine go build -o pendulum *.go
