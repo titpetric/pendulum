a5240b635992a059ba9fcf155f168ec2a81548da
Tit Petric <black@scene-si.org>
2017-08-21 11:15:19 +0200

    release build to github

--- codeship-release.sh
+++ codeship-release.sh
@@ -17,6 +17,41 @@ if [ ! -f "/root/.docker/config.json" ]; then
 	docker login -u $DOCKER_REGISTRY_USERNAME -p $DOCKER_REGISTRY_PASSWORD
 fi
 
+function github_release {
+	TAG="$1"
+	NAME="$2"
+	latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
+	comparison="$latest_tag..HEAD"
+	if [ -z "$latest_tag" ]; then
+		comparison="";
+	fi
+	changelog=$(git log $comparison --oneline --no-merges)
+	echo "Creating release $1: $2"
+	github-release release \
+		--user titpetric \
+		--repo pendulum \
+		--tag "$1" \
+		--name "$2" \
+		--description "$changelog"
+}
+
+function github_upload {
+	echo "Uploading $2 to $1"
+	github-release upload \
+		--user titpetric \
+		--repo pendulum \
+		--tag "$1" \
+		--name "$(basename $2)" \
+		--file "$2"
+}
+
+## Release to GitHub
+github_release ${CI_TAG_AUTO} "$(date)"
+FILES=$(find build -type f | grep tgz$)
+for FILE in $FILES; do
+	github_upload ${CI_TAG_AUTO} "$FILE"
+done
+
 ## Release to Docker Hub
 docker tag titpetric/pendulum titpetric/pendulum:${CI_COMMIT_ID_SHORT}
 docker push titpetric/pendulum:${CI_COMMIT_ID_SHORT}
