02bac4778ab25031c6bde3d46681824ec44bd2a6
Tit <black@scene-si.org>
2017-08-25 14:35:45 +0200

    bump alpine version

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ ! -d "vendor/github.com/elazarl/go-bindata-assetfs" ]; then
 	gvt fetch github.com/elazarl/go-bindata-assetfs
 fi
 go generate
-docker run --rm -it -e CGO_ENABLED=0 -v `pwd`:/go/src/app -w /go/src/app golang:1.8-alpine go build -o pendulum *.go
+docker run --rm -it -e CGO_ENABLED=0 -v `pwd`:/go/src/app -w /go/src/app golang:1.9-alpine go build -o pendulum *.go
